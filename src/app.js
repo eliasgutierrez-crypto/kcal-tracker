cf0e3ef7538dcab3d9dc83d59de59fc8a21cdb3b
Juan E <elias.gutierrez@alumnos.udg.mx>
2026-02-12 15:41:17 -0600

    Corregir orden de express.static

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-app.use(express.static('public'));
 const express = require('express');
 const cors = require('cors');
 const usersRoutes = require('./routes/users.routes');
@@ -8,24 +7,26 @@ const foodLogsRoutes = require('./routes/foodLogs.routes');
 const activitiesRoutes = require('./routes/activities.routes');
 const activityLogsRoutes = require('./routes/activityLogs.routes');
 
-
-
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public')); // <--- aquí después de app
+
+// Rutas
 app.use('/api/users', usersRoutes);
 app.use('/api/foods', foodsRoutes);
 app.use('/api/food-logs', foodLogsRoutes);
 app.use('/api/activities', activitiesRoutes);
 app.use('/api/activity-logs', activityLogsRoutes);
 
+// Ruta de prueba
 app.get('/', (req, res) => {
   res.json({ message: 'API kcal-tracker funcionando correctamente' });
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
